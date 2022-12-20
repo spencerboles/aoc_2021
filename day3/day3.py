17de239c34a92de6d3109984220d9f3a6a6aac89
Spencer Boles <48936680+spencerboles@users.noreply.github.com>
2022-12-19 22:56:19 -0700

    add comment

--- day3/day3.py
+++ day3/day3.py
@@ -1,22 +1,22 @@
 from msilib.schema import Binary
 
 
-def main(): 
-    fin = open('report.txt') 
+def main():
+    fin = open('report.txt')
     ones = []
-    zeros = [] 
+    zeros = []
 
 
-    for i in range(12): 
+    for i in range(12):
         ones.append(0)
-        zeros.append(0) 
+        zeros.append(0)
 
-    for r in fin: 
-        for i in range(len(r)-1): 
+    for r in fin:
+        for i in range(len(r)-1):
            # print(f"{i} {r[i]} ", end='')
 
-            if r[i] == '0': 
-                zeros[i] += 1  
+            if r[i] == '0':
+                zeros[i] += 1
                 #print(f"Zeros: {zeros}")
             else:
                 ones[i] += 1
@@ -25,81 +25,83 @@ def main():
     gama_string = ''
     epsilon_string = ''
 
-    for i in range(len(zeros)): 
-        if zeros[i] > ones[i]:  
+    for i in range(len(zeros)):
+        if zeros[i] > ones[i]:
             gama_string += '0'
-            epsilon_string += '1' 
-        else: 
+            epsilon_string += '1'
+        else:
             gama_string += '1'
             epsilon_string += '0'
-    
-    print(zeros) 
+
+    print(zeros)
     print(ones)
-    print(gama_string)  
-    print(epsilon_string) 
+    print(gama_string)
+    print(epsilon_string)
 
     report = read_report('report.txt')
 
-    for i in range(12):  
+    for i in range(12):
         if len(report) > 1:
-            value = find_common_value(report, i) 
+            value = find_common_value(report, i)
             print(f"Value: {value}")
-            report = filter_value(report, value, i) 
-    
+            report = filter_value(report, value, i)
+
 
     ox_value = int(report[0], base=2)
 
     report = read_report('report.txt')
-    for i in range(12): 
-        if len(report) > 1: 
-            value = find_common_value(report, i)  
+    for i in range(12):
+        if len(report) > 1:
+            value = find_common_value(report, i)
             value = '1' if value == '0' else '0'
             print(f"Value: {value}")
-            report = filter_value(report, value, i)   
-    
-    co_value = int(report[0], base=2) 
+            report = filter_value(report, value, i)
+
+    co_value = int(report[0], base=2)
 
     print(co_value * ox_value)
-    
+
     #print(int(o2_gen_rate, base=2) * int(co2_gen_rate, base=2))
 
 
-def find_common_value(report, i): 
-    zeros = 0 
+def find_common_value(report, i):
+    zeros = 0
     ones = 0
 
-    for l in report:  
-        if l[i] == '0': 
-            zeros += 1 
-        else: 
-            ones += 1 
-    
-    if zeros > ones: 
+    for l in report:
+        if l[i] == '0':
+            zeros += 1
+        else:
+            ones += 1
+
+    if zeros > ones:
         return '0'
-    else: 
+    else:
         return '1'
 
 
 
-def filter_value(diagnostic_report, value, position):  
+def filter_value(diagnostic_report, value, position):
    # filter out values based on value variable
     diagnostic_report = list(filter(lambda x: True if x[position] == value else False, diagnostic_report))
-    
-    #print(diagnostic_report) 
+
+    #print(diagnostic_report)
     return(diagnostic_report)
 
-def read_report(report_name):  
+def read_report(report_name):
     #open file connection
-    fin = open(report_name)  
+    fin = open(report_name)
     #read in file lines and append to an array
     report = []
-    for l in fin: 
-        report.append(l.replace('\n', ''))  
-    
+    for l in fin:
+        report.append(l.replace('\n', ''))
+
     # return report as a list
     return report
 
 
 
-if __name__ == '__main__': 
+if __name__ == '__main__':
     main()
+
+#this is a test comment
